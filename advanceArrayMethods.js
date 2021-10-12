3929d9144dcfe0d4d39a15954f768a5cace932f9
Juan Yepes <yepes.j@hotmail.com>
2021-10-12 09:13:49 -0400

    added new subjects

---
+++ advanceArrayMethods.js
@@ -0,0 +1,25 @@
+arr = [5,12,50]
+
+arr.sort() // very tricky 
+
+arr.filter(item => {
+    return item.type === "shirt"
+})
+
+arr.find()
+
+arr.some()
+
+arr.every()
+
+arr.forEach()
+
+arr.map()
+
+arr.reduce()
+
+arr.reduceRight()
+
+
+
+
--- findModenotDONE.js
+++ findModenotDONE.js
@@ -1,9 +1,9 @@
 /// not done
-function findRepeatAmount(agez) {
-  let bucket = agez[0];
+function findRepeatAmount(age) {
+  let bucket = age[0];
   let counter = 0;
-  for (let i = 1; i < agez.length; i++) {
-    if (bucket === agez[i]) {
+  for (let i = 1; i < age.length; i++) {
+    if (bucket === age[i]) {
       counter++;
     }
   }
